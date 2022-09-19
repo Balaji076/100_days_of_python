c007fab830fd20eaf3781902b8d26cc3fb1cff7e
Balaji076 <112414746+Balaji076@users.noreply.github.com>
2022-09-19 23:56:14 +0530

    Add files via upload

---
+++ DaY-2.py
@@ -0,0 +1,9 @@
+J=int(input("ENTER THE FIRST NUMBER:"))
+K=int(input("ENTER THE SECOND NUMBER:"))
+if J>K :
+    print("THE GREATER NUMBER IS:",J)
+elif J==K:
+    print("BOTH ARE EQUAL")
+else:
+    print("THE GREATER NUMBER IS:",K)
+
