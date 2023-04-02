1dc4a038c541292d3c9d98a886f1033e857e5f1e
Balaji076 <112414746+Balaji076@users.noreply.github.com>
2023-04-02 22:56:51 +0530

    Add files via upload

---
+++ DAY_18.py
@@ -0,0 +1,7 @@
+print("TIMELINE ")
+i=int(input("ENTER YOUR YEAR OF BIRTH:"))
+if 1900<=i<2000:
+    print("YOU WERE BORN IN 20TH CENTUARY")
+elif 2000<=i<2100:
+    print("YOU WERE BORN IN 21ST CENTUARY")
+    
